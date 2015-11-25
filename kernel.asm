9dc98b2ff0a230abdeb0ea1bda760c012efccd75
Panagiotis Roubatsis <proubatsis@gmail.com>
2015-11-25 10:39:32 -0500

    New print_string function in io.asm.

--- kernel.asm
+++ kernel.asm
@@ -5,25 +5,11 @@ main:
     mov bp, 0x9000
     mov sp, bp
     
-    mov bx, hello_world
+    push hello_world
     call print_string
     
     jmp $
     
-; Print string starting from memory location in bx
-print_string:
-    mov ah, 0x0e
-    
-    .loop:
-        mov al, [bx]
-        cmp al, 0
-        je .done
-        
-        int 0x10
-        inc bx
-        jmp .loop
-        
-    .done:
-        ret
+%include 'kernel/io.asm'
     
 hello_world: db "Hello, World", 0
---
+++ kernel/io.asm
@@ -0,0 +1,29 @@
+; print_string(char*) -> Returns nothing
+; Print a string
+print_string:
+
+    ; Set up stack frame
+    push bp
+    mov bp, sp
+    
+    pusha ; Save all registers
+    
+    mov ah, 0x0e      ; 0x0e is the function to write a char to the screen with int 0x10
+    mov bx, [ebp + 4] ; Get the first argument
+    
+    .loop:
+        mov al, [bx]  ; Get the next char from bx
+        cmp al, 0     ; Are we at the end of the string?
+        je .done
+        
+        int 0x10      ; Print the character
+        inc bx        ; Increment address to point to the next char
+        jmp .loop
+    
+    .done:
+        pop bx ; Restore all registers
+        
+        mov sp, bp
+        pop bp
+        ret
+    
