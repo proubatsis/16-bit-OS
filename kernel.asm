bcd2ad913f57c39d5bbcbd35224e077363489843
Panagiotis Roubatsis <proubatsis@gmail.com>
2015-12-02 10:05:23 -0500

    Added memcpy function.

--- kernel.asm
+++ kernel.asm
@@ -11,25 +11,23 @@ main:
     push word file_table_sector
     call load_file_table
     
-    push filename
-    call lookup_file
-    
-    cmp ax, 0
-    je file_not_found
-    
+    ;copy contents of filename to test_copy
+    push word 9
+    push test_copy
+    push word 0
     push found_msg
+    push word 0
+    call memcpy
+    
+    push test_copy
     call print_string
     
-    jmp $
-    
-file_not_found:
-    push no_file_msg
-    call print_string
     jmp $
     
 %include 'kernel/std_io.asm'
 %include 'kernel/file_io.asm'
 %include 'kernel/strings.asm'
+%include 'kernel/memory.asm'
 
 welcome_message db "Welcome to my OS!", 0x0D, 0x0A, 0x00
 
@@ -40,5 +38,6 @@ file_input: times 32 db 0
 input_buffer: times 32 db 0
 
 filename db "stuff.bin", 0
+test_copy: times 32 db 0
 
 file_table_sector equ 3
