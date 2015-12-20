66bcfc4a7e76699dc3531ef2937ff1ba42e694b2
Panagiotis Roubatsis <proubatsis@gmail.com>
2015-12-19 21:09:50 -0500

    Cleaned up main function.

--- kernel.asm
+++ kernel.asm
@@ -4,44 +4,17 @@
 main:
     mov bp, 0xFFFF
     mov sp, bp
-    sub sp, 4
     
     push welcome_message
     call print_string
     
-    push word 512
-    call malloc
-    
-    mov bx, ax
-    mov [bp - 2], bx
-    
-    push word [bx]
-    push word malloc_segment
+    push file_buffer
+    push word 0
     push filename
     call open_file
     
-    mov bx, [bp - 2]
-    mov [bp - 4], ax
-    
-    push word 32
-    push file_buffer
-    push word 0
-    push word [bx]
-    push word malloc_segment
-    call memcpy
-    
-    mov ax, [bp - 4]
-    
-    ; add null terminator to string
-    mov bx, ax
-    add bx, file_buffer
-    mov [bx], word 0
-    
     push file_buffer
     call print_string
-    
-    push word [bp - 2]
-    call free
     
     jmp $
     
